5cc025596e422b712b50868d13a6be3ceacd3713
dikxak <dikshakpoudel76@gmail.com>
2022-09-14 15:37:59 +0545

    complete basic input and form validation

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,103 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+import useInput from '../../hooks/use-input';
 
 import classes from './Form.module.css';
 
 const Form = props => {
+  const nameRef = useRef();
+
+  const {
+    enteredInput: nameInput,
+    inputHasError: nameHasError,
+    inputIsValid: nameIsValid,
+    inputChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    errorMessage: nameErrorMessage,
+    resetInput: resetName,
+  } = useInput(value => {
+    return {
+      inputIsValid: value.trim().length !== 0,
+      errorMessage: 'Country name can not be empty.',
+    };
+  });
+
+  const {
+    enteredInput: capitalInput,
+    inputHasError: capitalHasError,
+    inputIsValid: capitalIsValid,
+    inputChangeHandler: capitalChangeHandler,
+    inputBlurHandler: capitalBlurHandler,
+    errorMessage: capitalErrorMessage,
+    resetInput: resetCapital,
+  } = useInput(value => {
+    return {
+      inputIsValid: value.trim().length !== 0,
+      errorMessage: 'Country capital can not be empty.',
+    };
+  });
+
+  const {
+    enteredInput: populationInput,
+    inputHasError: populationHasError,
+    inputIsValid: populationIsValid,
+    inputChangeHandler: populationChangeHandler,
+    inputBlurHandler: populationBlurHandler,
+    errorMessage: populationErrorMessage,
+    resetInput: resetPopulation,
+  } = useInput(value => {
+    const inputLengthIsValid = value.trim().length !== 0;
+    const inputValueIsValid = +value > 0;
+
+    const inputIsValid = inputLengthIsValid && inputValueIsValid;
+
+    const errorMessage = !inputLengthIsValid
+      ? 'Country population can not be empty.'
+      : !inputValueIsValid
+      ? 'Country population can not be less than 1.'
+      : '';
+    return {
+      inputIsValid,
+      errorMessage,
+    };
+  });
+
+  const formIsValid = nameIsValid && capitalIsValid && populationIsValid;
+
+  const formSubmitHandler = e => {
+    e.preventDefault();
+
+    nameBlurHandler(e);
+    capitalBlurHandler(e);
+    populationBlurHandler(e);
+
+    if (!formIsValid) return;
+
+    console.log(nameInput, capitalInput, populationInput);
+
+    nameRef.current.focus();
+
+    resetName();
+    resetCapital();
+    resetPopulation();
+  };
+
   return (
-    <form className={classes['form']}>
+    <form onSubmit={formSubmitHandler} className={classes['form']}>
       <div className={classes['form-control-group']}>
         <div className={classes['form-group']}>
           <label htmlFor="name">Enter name:</label>
-          <input type="text" id="name" name="name" placeholder="e.g. Nepal" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            ref={nameRef}
+            placeholder="e.g. Nepal"
+            value={nameInput}
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+          />
+          {nameHasError && <p className="error-msg">{nameErrorMessage}</p>}
         </div>
 
         <div className={classes['form-group']}>
@@ -18,7 +107,13 @@ const Form = props => {
             id="population"
             name="population"
             placeholder="e.g. 30286125"
+            value={populationInput}
+            onChange={populationChangeHandler}
+            onBlur={populationBlurHandler}
           />
+          {populationHasError && (
+            <p className="error-msg">{populationErrorMessage}</p>
+          )}
         </div>
 
         <div className={classes['form-group']}>
@@ -28,7 +123,13 @@ const Form = props => {
             id="capital"
             name="capital"
             placeholder="e.g. Kathmandu"
+            value={capitalInput}
+            onChange={capitalChangeHandler}
+            onBlur={capitalBlurHandler}
           />
+          {capitalHasError && (
+            <p className="error-msg">{capitalErrorMessage}</p>
+          )}
         </div>
       </div>
 
